33b089f8865f504a35bc94a267586862d1c28c32
Hepic <hepic135@gmail.com>
2018-05-20 16:28:37 +0300

    Create cross validation

--- classify.py
+++ classify.py
@@ -1,8 +1,29 @@
 import pandas as pd
+import numpy as np
 from knn import KNN 
 from ast import literal_eval
 from distances import DTW
 from sklearn import svm, preprocessing
+from sklearn.model_selection import cross_val_score, KFold
+from sklearn.metrics import accuracy_score
+
+
+def crossValidation(clf, allSequences, categoryIds, le):
+    splits, accur = 4, 0
+    kf = KFold(n_splits=splits)
+    
+    # Splits data to train and test data
+    for trainIndex, testIndex in kf.split(allSequences):
+        kfSeqTrain = [allSequences[ind] for ind in trainIndex]
+        kfSeqTest = [allSequences[ind] for ind in testIndex]
+    
+        clf.fit(kfSeqTrain, categoryIds[trainIndex])
+        kfTestPredIds = clf.predict(kfSeqTest)
+        
+        accur += accuracy_score(categoryIds[testIndex], kfTestPredIds)
+    
+    accur /= float(splits)
+    print 'Accuracy: ', accur
 
 
 def main():
@@ -12,6 +33,8 @@ def main():
     testSet = pd.read_csv('datasets/test_set_a1.csv',
                           converters={'Trajectory': literal_eval})
     
+    trainSet = trainSet[:40]
+
     # labels for categories
     le = preprocessing.LabelEncoder()
     categoryIds = le.fit_transform(trainSet['journeyPatternId'])   
@@ -29,7 +52,7 @@ def main():
     predIds = clf.predict(testSet['Trajectory'])
     predCategs = le.inverse_transform(predIds)
 
-    print predCategs
+    crossValidation(clf, allSequences, categoryIds, le)
 
     
 if __name__ == '__main__':
